c083f6d09a4a1f681be20ca3e086c5256fe0757b
Paulo G Reis <paulosantos--@live.com>
2022-02-16 01:42:05 -0400

    Configurando ambiente de dev e prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
     mode: 'development',
+    devtool: isDevelopment ? 'eval-source-map': 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
